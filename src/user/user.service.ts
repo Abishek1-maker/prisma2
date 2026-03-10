bbd8213728ea82121bc615091fdf8f1aad008526
Abishek kunwar <kesu.kunwar@gmail.com>
2026-03-10 12:57:58 +0545

    Hashed password npm install bcrypt @types/bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async create(Data: CreateUserDto) {
+    if (Data.password) {
+      Data.password = await bcrypt.hash(Data.password, 10);
+    }
     return await this.prisma.user.create({ data: Data });
   }
 
@@ -19,8 +23,11 @@ export class UserService {
     return this.prisma.user.findUnique({ where: { id } });
   }
 
-  update(id: number, Data: UpdateUserDto) {
-    return this.prisma.user.update({ where: { id }, data: Data });
+  async update(id: number, Data: UpdateUserDto) {
+    if (Data.password) {
+      Data.password = await bcrypt.hash(Data.password, 10);
+    }
+    return await this.prisma.user.update({ where: { id }, data: Data });
   }
 
   remove(id: number) {
